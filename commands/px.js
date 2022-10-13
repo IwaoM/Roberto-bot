7bedf5f3ca6a0696b4755c5d4281fa9920f0c78a
“IwaoM” <iwao27@gmail.com>
2022-10-13 11:23:34 +0200

    implemented px, which is arguably the most important feature of the entire bot

--- commands/px.js
+++ commands/px.js
@@ -1,13 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const { generatePhoenix } = require("../helpers/misc.helper.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("px")
-		.setDescription("Writes 'Phoenix'"),
+  data: new SlashCommandBuilder()
+    .setName("px")
+    .setDescription("Alternate ways to spell the name of that fire guy in Valorant"),
 
-	async execute(interaction) {
-		await interaction.reply("Px");
-	},
+  async execute (interaction) {
+    const againButton = new ButtonBuilder().setCustomId("px_again").setLabel("Again!").setStyle(ButtonStyle.Primary);
+    const buttonRow = new ActionRowBuilder().addComponents(againButton);
 
-  usage: "pxpxpxpxpxpx"
+    let name = generatePhoenix();
+    await interaction.reply({ content: name, components: [buttonRow] });
+  },
+
+  async executeButton (interaction) {
+    const name = generatePhoenix();
+    await interaction.update(name);
+  },
+
+  usage: `• \`/px\`: generates a new variant of the name
+• Tap the *Again!* button to regenerate`
 };
--- helpers/misc.helper.js
+++ helpers/misc.helper.js
@@ -133,5 +133,54 @@ module.exports = {
       return false;
 
     }
+  },
+
+  generatePhoenix () {
+    // the idea here is to scramble center letters more than outer letters
+    // 'p' and 'x' will be placed correctly most of the time, whereas 'o', 'e', 'n' will often be misplaced
+
+    // inner letters
+    let letters = ["o", "e", "n"];
+    let indexToRemove;
+    let scrambleAmount = Math.floor(Math.random() * 3) + 1;
+
+    for (let i = 0; i < scrambleAmount; i++) {
+      const indexToSwap = Math.floor(Math.random() * (letters.length - 1));
+      [letters[indexToSwap], letters[indexToSwap + 1]] = [letters[indexToSwap + 1], letters[indexToSwap]];
+    }
+
+    if (!Math.floor(Math.random() * 3)) {
+      indexToRemove = Math.floor(Math.random() * letters.length);
+      letters.splice(indexToRemove, 1);
+    }
+
+    // center letters
+    letters = ["h", ...letters, "i"];
+    scrambleAmount = Math.floor(Math.random() * 3) + 1;
+
+    for (let i = 0; i < scrambleAmount; i++) {
+      const indexToSwap = Math.floor(Math.random() * (letters.length - 1));
+      [letters[indexToSwap], letters[indexToSwap + 1]] = [letters[indexToSwap + 1], letters[indexToSwap]];
+    }
+
+    if (!Math.floor(Math.random() * 3)) {
+      indexToRemove = Math.floor(Math.random() * letters.length);
+      letters.splice(indexToRemove, 1);
+    }
+
+    // outer letters
+    letters = ["p", ...letters, "x"];
+    scrambleAmount = Math.floor(Math.random() * 3) + 1;
+
+    for (let i = 0; i < scrambleAmount; i++) {
+      const indexToSwap = Math.floor(Math.random() * (letters.length - 1));
+      [letters[indexToSwap], letters[indexToSwap + 1]] = [letters[indexToSwap + 1], letters[indexToSwap]];
+    }
+
+    if (!Math.floor(Math.random() * 3)) {
+      indexToRemove = Math.floor(Math.random() * (letters.length - 1)) + 1;
+      letters.splice(indexToRemove, 1);
+    }
+    return letters.join("");
   }
 };
